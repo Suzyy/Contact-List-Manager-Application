835a9232813d2060ed97dea50cfd93dfef474be9
Suzyy <35048564+Suzyy@users.noreply.github.com>
2023-10-04 20:32:54 +0000

    Recreate app

--- clmp/src/main/java/com/example/clmp/ClmpApplication.java
+++ clmp/src/main/java/com/example/clmp/ClmpApplication.java
@@ -1,16 +1,13 @@
-package com.example.demo;
+package com.example.clmp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-
 @SpringBootApplication
-public class DemoApplication {
+public class ClmpApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ClmpApplication.class, args);
 	}
 
 }
-
