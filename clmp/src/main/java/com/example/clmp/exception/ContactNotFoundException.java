37e0e09602b94c315580db88baa2e660acdccb02
Suzyy <35048564+Suzyy@users.noreply.github.com>
2023-10-06 15:17:18 +0000

    Created exception classes for contact

---
+++ clmp/src/main/java/com/example/clmp/exception/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.clmp.exception;
+
+public class ContactNotFoundException extends RuntimeException {
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ clmp/src/main/java/com/example/clmp/exception/ContactNotValidException.java
@@ -0,0 +1,7 @@
+package com.example.clmp.exception;
+
+public class ContactNotValidException extends RuntimeException {
+    public ContactNotValidException(String message) {
+        super(message);
+    }  
+}
