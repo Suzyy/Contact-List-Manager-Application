cf8c7da5b692a4d3b71510891ee6550f23965234
Suzyy <35048564+Suzyy@users.noreply.github.com>
2023-10-08 17:59:40 +0000

    Registered jwt filter to Security config

--- clmp/src/main/java/com/example/clmp/config/SecurityConfig.java
+++ clmp/src/main/java/com/example/clmp/config/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.clmp.filter.JwtFilter;
 import com.example.clmp.repo.UserRepo;
 import com.example.clmp.service.CustomUserDetailsService;
 
@@ -25,6 +28,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
+
     //@Override
     //protected void configure(HttpSecurity httpSecurity) throws Exception {
     //    httpSecurity.authorizeRequests().antMatchers("/").permitAll().and().authorizeRequests().antMatchers("/console/**").permitAll();
@@ -52,7 +59,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest().authenticated();
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+            .permitAll().anyRequest().authenticated()
+            .and().exceptionHandling().and().sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
-    
 }
