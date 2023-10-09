895aa4c9f63dcfad78035348283b92c6dbf3f647
Suzyy <35048564+Suzyy@users.noreply.github.com>
2023-10-09 15:48:26 +0000

    Wrote unit test for getAllContacts

--- clmp/src/test/java/com/example/clmp/service/ContactServiceTest.java
+++ clmp/src/test/java/com/example/clmp/service/ContactServiceTest.java
@@ -1,12 +1,21 @@
 package com.example.clmp.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.clmp.repo.ContactRepo;
+import com.example.clmp.dto.ContactDTO;
+import com.example.clmp.entity.Contact;
 
 @SpringBootTest
 public class ContactServiceTest {
@@ -16,4 +25,35 @@ public class ContactServiceTest {
 
     @InjectMocks
     private ContactService contactService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllContact() {
+        //Creating mock contacts
+        List<Contact> mockContactList = new ArrayList<>();
+        mockContactList.add(new Contact(null, "Suzy", "Lee", "suzy@example.com", "123 Yonge St", null));
+        mockContactList.add(new Contact(null, "Jay", "Lee", "jay@example.com", "456 Yonge St", null));
+
+        //Mocking findAll() method in repo
+        when(contactRepo.findAll()).thenReturn(mockContactList);
+
+        //Calling contact service
+        List<ContactDTO> contactDTOs = contactService.getAllContacts();
+
+        assertEquals(2, contactDTOs.size());
+        assertEquals("Suzy", contactDTOs.get(0).getFirstName());
+        assertEquals("Lee", contactDTOs.get(0).getLastName());
+        assertEquals("suzy@example.com", contactDTOs.get(0).getEmail());
+        assertEquals("123 Yonge St", contactDTOs.get(0).getAddress());
+        assertEquals("Jay", contactDTOs.get(1).getFirstName());
+        assertEquals("Lee", contactDTOs.get(1).getLastName());
+        assertEquals("jay@example.com", contactDTOs.get(1).getEmail());
+        assertEquals("456 Yonge St", contactDTOs.get(1).getAddress());
+    }
+
+
 }
