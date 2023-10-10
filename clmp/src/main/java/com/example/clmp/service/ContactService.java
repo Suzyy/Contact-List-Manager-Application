fa0e13c23630e7c1a85f1a8d433209be23e4feb1
Suzyy <35048564+Suzyy@users.noreply.github.com>
2023-10-10 15:48:47 +0000

    Added specific comments to both contact and notes services

--- clmp/src/main/java/com/example/clmp/service/ContactService.java
+++ clmp/src/main/java/com/example/clmp/service/ContactService.java
@@ -22,24 +22,29 @@ public class ContactService {
     @Autowired
     private ContactRepo contactRepo;
 
+    //DESCRIPTION: Getting all contacts and returning a list of contactDTOs to the controller
     public List<ContactDTO> getAllContacts() {
         List<Contact> contactList = contactRepo.findAll();
         return contactList.stream().map(this::convertToDTO).collect(Collectors.toList());
     
     }
 
+    //DESCRIPTION: Getting contact by specific contact id
     public Optional<ContactDTO> getContactById(Long id) {
         Optional<Contact> contactData = contactRepo.findById(id);
 
+        //If contact exists, return contactDtO to the controller
         if (contactData.isPresent()) {
             return Optional.of(convertToDTO(contactData.get()));
-        } else {
+        } else { //If contact does not exist, throw contact not found exception
             throw new ContactNotFoundException("Contact not found with ID: " + id);
         }
     }
 
+    //DESCRIPTION: Adding contact to the contact list database
     public ContactDTO addContact(ContactDTO contactDTO) {
         //Validation check: check if required fields are not null
+        //If required fiedls are null, throw contact not valid exception
         if (contactDTO.getFirstName() == null || contactDTO.getLastName() == null || contactDTO.getPhoneNumber() == null || contactDTO.getEmail() == null) {
             throw new ContactNotValidException("Contact data is not valid. First name, last name, and email are required.");
         }
@@ -49,10 +54,12 @@ public class ContactService {
         return convertToDTO(savedContact);
     }
 
+    //DESCRIPTION: Updating contact by specific contact id
     public Optional<ContactDTO> updateContactById(Long id, ContactDTO newContactDTO) {
         Optional<Contact> oldContactData = contactRepo.findById(id);
         Contact newContactData = convertToEntity(newContactDTO);
 
+        //If contact with the specific id exists, update fields and return saved contactDTO to controller
         if (oldContactData.isPresent()) {
             Contact updatedContactData = oldContactData.get();
             updatedContactData.setFirstName(newContactData.getFirstName());
@@ -70,9 +77,11 @@ public class ContactService {
         }
     }
 
+    //DESCRIPTION: Deleting contact by specific contact id
     public void deleteContactById(Long id) {
         Optional<Contact> contactData = contactRepo.findById(id);
         
+        //If contact with the specific id exists, delete contact
         if (contactData.isPresent()) {
             contactRepo.deleteById(id);
         } else {
--- clmp/src/main/java/com/example/clmp/service/NotesService.java
+++ clmp/src/main/java/com/example/clmp/service/NotesService.java
@@ -21,31 +21,38 @@ public class NotesService {
     @Autowired
     private NotesRepo notesRepo;
 
+    //Getting notes by specific note id
     public Optional<NotesDTO> getNotesById(Long id) {
         Optional<Notes> notesData = notesRepo.findById(id);
 
+        //If note with the specific id exists, return note data to controller
         if (notesData.isPresent()) {
             return Optional.of(convertToDTO(notesData.get()));
-        } else {
+        } else { //if note does not exists, throw notest not found exception
             throw new NotesNotFoundException("Notes not found with ID: " + id);
         }
     }
 
+    //Adding notes
     public NotesDTO addNotes(NotesDTO notesDTO) {
         //Validation check: check if required fields are not null
+        //If required fields are null, throw note not valid exception to controller
         if (notesDTO.getNoteText() == null || notesDTO.getContactId() == null) {
             throw new NotesNotValidException("Note data is not valid. NoteText and ContactID are required.");
         }
+        //Note is valid, add notes to the database
         Notes notes = convertToEntity(notesDTO);
         Notes savedNotes = notesRepo.save(notes);
 
         return convertToDTO(savedNotes);
     }
 
+    //Updating notes by specific note id
     public Optional<NotesDTO> updateNotesById(Long id, NotesDTO newNotesDTO) {
         Optional<Notes> oldNotesData = notesRepo.findById(id);
         Notes newNotesData = convertToEntity(newNotesDTO);
 
+        //If note with note id exists, update fields and save
         if (oldNotesData.isPresent()) {
             Notes updatedNotesData = oldNotesData.get();
             updatedNotesData.setContactId(newNotesData.getContactId());
@@ -54,18 +61,20 @@ public class NotesService {
 
             Notes savedNotes = notesRepo.save(updatedNotesData);
             return Optional.of(convertToDTO(savedNotes));
-        } else {
+        } else { //If note with not id does not exist, throw notes not found exception
             //exception where note is not found with the id given: no note to update
             throw new NotesNotFoundException("Notes not found with ID: " + id);
         }
     }
 
+    //Deleting note by specific note id
     public void deleteNotesById(Long id) {
         Optional<Notes> notesData = notesRepo.findById(id);
         
+        //If note with note id exists, delete
         if (notesData.isPresent()) {
             notesRepo.deleteById(id);
-        } else {
+        } else { //If note with note id does not exists, throw notes not found exception
             //exception where note is not found with the id given: no note to delete
             throw new NotesNotFoundException("Notes not found with ID: " + id);
         }
